9256e2a153a604cd93d74e47ba5cc4e83de14154
ldh7228 <ldh7228@gmail.com>
2021-08-19 14:58:46 +0900

    Fix num

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const number = document.getElementById('number');
 const start = document.getElementById('start');
 
+let game = false;
+
 let input;
 let num = [];
+let initialValue = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
@@ -11,11 +14,17 @@ function getRandomIntInclusive(min, max) {
 }
 
 function startEvent() {
-    input = document.getElementById('input').value;
-    for (let i = 0; i < input; i++) {
-        num[i] = (getRandomIntInclusive(0, 9));
-        console.log(num[i]);
+    if (!game) {
+        let n = 0;
+        input = document.getElementById('input').value;
+        for (let i = 0; i < input; i++) {
+            let randomNumber = getRandomIntInclusive(0, 9 - n)
+            num[i] = (initialValue[randomNumber]);
+            initialValue.splice(randomNumber, 1);
+            console.log(num[i]);
+            n++;
+        }
+        game = true;
     }
 }
-
-start.addEventListener('click', startEvent);
+    start.addEventListener('click', startEvent);
