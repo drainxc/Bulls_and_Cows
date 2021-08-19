b35d344de82d494595c870c7a9c93653f7e301f4
ldh7228 <ldh7228@gmail.com>
2021-08-19 14:50:20 +0900

    Add num

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-const input = document.getElementById('input').value;
 const number = document.getElementById('number');
 const start = document.getElementById('start');
+
+let input;
+let num = [];
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
@@ -8,3 +10,12 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function startEvent() {
+    input = document.getElementById('input').value;
+    for (let i = 0; i < input; i++) {
+        num[i] = (getRandomIntInclusive(0, 9));
+        console.log(num[i]);
+    }
+}
+
+start.addEventListener('click', startEvent);
