ff87f1956aa5e22fec11333f497d557b1a00f897
ldh7228 <ldh7228@gmail.com>
2021-08-19 14:42:49 +0900

    Add random

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-const input = document.getElementById('input');
+const input = document.getElementById('input').value;
 const number = document.getElementById('number');
 const start = document.getElementById('start');
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
